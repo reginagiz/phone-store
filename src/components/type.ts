2ffe061dcb948f1a7558c9377a0c4f83295f6938
Regina <reginagiz98@gmail.com>
2023-11-10 12:16:00 +0500

    create types

---
+++ src/components/type.ts
@@ -0,0 +1,10 @@
+export type Phone = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  colors: string[];
+  popularity: number;
+  desc: string;
+};
+export type Phones = Phone[];
