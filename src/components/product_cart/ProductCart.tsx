496c20ec34f67f5e1aab7d52629cc3cdc7861feb
Regina <reginagiz98@gmail.com>
2023-11-13 10:28:04 +0500

    add to cart functionality

--- src/components/product_cart/ProductCart.tsx
+++ src/components/product_cart/ProductCart.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Phone } from "../type";
 import s from "./ProductCart.module.css";
 import { Link } from "react-router-dom";
+import cartStore from "../../store/CartStore";
 
 type ProductItemProps = {
   product: Phone;
@@ -16,8 +17,13 @@ const ProductCart: React.FC<ProductItemProps> = ({ product }) => {
     green: s.greenPhone,
   };
 
+  const handleAddToCart = (product: Phone) => {
+    cartStore.addToCart(product);
+  };
+
   return (
     <>
+      <hr />
       <div key={product.id} className={s.product_cart}>
         <div className={s.image_container}>
           <img src={product.image} alt="phone_img"></img>
@@ -25,7 +31,9 @@ const ProductCart: React.FC<ProductItemProps> = ({ product }) => {
         <div className={s.info_container}>
           <div className={s.general_info}>
             <div className={s.name_color}>
-              <h3>{product.name}</h3>
+              <Link to={`/api/products/${product.id}`}>
+                <h3>{product.name}</h3>
+              </Link>
               <div className={s.colors_container}>
                 {product.colors.map((color) => (
                   <div
@@ -38,16 +46,22 @@ const ProductCart: React.FC<ProductItemProps> = ({ product }) => {
               </div>
             </div>
             <Link to={`/api/products/${product.id}`}>
-              <button className="secondary_button">Подробнее</button>
+              <button className={`secondary_button ${s.sec_button}`}>
+                Подробнее
+              </button>
             </Link>
           </div>
           <div className={s.price_cart}>
             <h3>{product.price} ₽</h3>
-            <button className="primary_button">В корзину</button>
+            <button
+              className="primary_button"
+              onClick={() => handleAddToCart(product)}
+            >
+              В корзину
+            </button>
           </div>
         </div>
       </div>
-      <hr />
     </>
   );
 };
