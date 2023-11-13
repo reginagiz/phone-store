42bbc8ee0353e7b457c8fd693a7d2ae2e4c9c3dd
Regina <reginagiz98@gmail.com>
2023-11-13 10:22:55 +0500

    add icons

---
+++ src/components/images/BackIcon.tsx
@@ -0,0 +1,20 @@
+const BackIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M15 19L8 12L15 5"
+        stroke="#A3A7BF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default BackIcon;
---
+++ src/components/images/SortIcon.tsx
@@ -0,0 +1,20 @@
+const SortIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M20.25 6.75H3.75M16.25 12H3.75M12 17.25H3.75"
+        stroke="#798FFF"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default SortIcon;
