45adeefa24ccf0bc5f0d5162298cba5b515a9998
Regina <reginagiz98@gmail.com>
2023-11-13 10:19:07 +0500

    add links and badge

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,29 @@
+import { Link } from "react-router-dom";
 import CartIcon from "../images/CartIcon";
 import s from "./Header.module.css";
+import cartStore from "../../store/CartStore";
+import { observer } from "mobx-react-lite";
 
 const Header = () => {
   return (
     <div className={s.header}>
       <div className={s.header_container}>
-        <h2 className={s.logo}>graff.shop</h2>
-        <div className={s.cart}>
-          <CartIcon />
-        </div>
+        <Link to={"/"}>
+          <h2 className={s.logo}>graff.shop</h2>
+        </Link>
+        <Link to={"/cart"}>
+          <div className={s.cart}>
+            <CartIcon />
+            {cartStore.totalQuantity > 0 ? (
+              <div className={s.badge}>{cartStore.totalQuantity}</div>
+            ) : (
+              ""
+            )}
+          </div>
+        </Link>
       </div>
     </div>
   );
 };
 
-export default Header;
+export default observer(Header);
